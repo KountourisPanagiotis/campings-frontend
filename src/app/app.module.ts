88abde33978ed8a4da237c12d732c63904dae6a7
Kountouris Pan <kountourispan022@gmail.com>
2023-06-14 00:03:45 +0300

    prior to components creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule , Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
-
+// const routes: Routes = [
+// 	{ path: 'path_to_A', component: AComponent },
+// 	{ path: '', component: WelcomeComponent },
+// 	{ path: '**', component: PageNotFoundComponent },
+// ];
 
 
 @NgModule({
