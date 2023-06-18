e43d96e467ea23400d23813a7abb23b8ada17904
Kountouris Pan <kountourispan022@gmail.com>
2023-06-18 17:47:04 +0300

    bookCode set implemented

--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClientTransactionService } from '../../services/client-transaction/client-transaction.service';
+import { SpotrentalService } from '../../services/spotrental/spotrental.service';
+import { BookingService } from '../../services/booking/booking.service';
+import { IBooking } from '../../models/booking.model';
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.css']
 })
-export class BookingComponent {
+export class BookingComponent implements OnInit {
+  bookCode: number = 0; // Initialize bookCode with a default value
 
+  constructor(
+    private clientTransactionService: ClientTransactionService,
+    private spotrentalService: SpotrentalService,
+    private bookingService: BookingService
+  ) {}
+
+  ngOnInit(): void {
+    this.generateBookCode();
+  }
+
+  generateBookCode(): void {
+    this.bookingService.getAllBookings().subscribe((bookings: IBooking[]) => {
+      const lastBooking = bookings[bookings.length - 1]; // Get the last entry
+      this.bookCode = lastBooking.bookCode + 1; // Increment the bookCode by 1
+    });
+  }
+  
+
+  // Rest of the component code...
 }
